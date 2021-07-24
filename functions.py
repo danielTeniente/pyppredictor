37264f674e88bbd3bde10381161023560c7b7791
Daniel Diaz <daniel.diaz.bedoya@outlook.com>
2021-07-23 19:12:40 -0500

    (testing function): Test cases prepared

---
+++ functions.py
@@ -0,0 +1,6 @@
+#should verify that the hour is correct
+# the hour should be expressed in 24 h
+# format: hh:mm:ss
+
+def verify_hour(hour):
+    return True
---
+++ test.py
@@ -0,0 +1,42 @@
+# testing file
+
+import unittest
+import functions 
+
+class TestSum(unittest.TestCase):
+    #testing verify hour function
+    def test_vh_hour(self):
+        module_response = functions.verify_hour(hour='25:30:17')
+        #solution
+        sol = False
+        self.assertEqual(module_response, sol, 
+            f'Area is shown {module_response} rather than {sol}')
+    def test_vh_minutes(self):
+        module_response = functions.verify_hour(hour='03:65:17')
+        #solution
+        sol = False
+        self.assertEqual(module_response, sol, 
+            f'Area is shown {module_response} rather than {sol}')
+    def test_vh_seconds(self):
+        module_response = functions.verify_hour(hour='12:30:70')
+        #solution
+        sol = False
+        self.assertEqual(module_response, sol, 
+            f'Area is shown {module_response} rather than {sol}')
+        
+    def test_vh_format(self):
+            module_response = functions.verify_hour(hour='2:0:0')
+            #solution
+            sol = False
+            self.assertEqual(module_response, sol, 
+                f'Area is shown {module_response} rather than {sol}')
+
+    def test_vh_numbers(self):
+            module_response = functions.verify_hour(hour='2:0:0')
+            #solution
+            sol = False
+            self.assertEqual(module_response, sol, 
+                f'Area is shown {module_response} rather than {sol}')
+        
+if __name__ == '__main__':
+    unittest.main()
